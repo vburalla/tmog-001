e052a19ee27869d42918c40ac8be32032b97dd69
Víctor Buralla Blasco <vburalla@mercadona.es>
2024-02-05 21:54:38 +0100

    fix: vbb solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,32 @@
 package org.tfoc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public int[] twoSum(
+    public static int[] twoSum(
             int[] nums,
             int target) {
 
-        return null;
+        int[] solution = new int[]{};
+        if(nums.length > 0) {
+
+            Map<Integer, Integer> visited = new HashMap<>();
+            visited.put(target - nums[0], 0);
+            int i = 1;
+            boolean found = false;
+            while(i < nums.length && !(found = visited.containsKey(nums[i]))) {
+                    visited.put(target - nums[i], i);
+                    i++;
+            }
+            if(found)
+                solution = new int[]{visited.get(nums[i]), i};
+        }
+        return solution;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,36 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.tfoc.utils.IntArrayConverter;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 class SolutionTest {
 
     @Test
-    void testSolution() {
+    @DisplayName("Should return empty array for empty input")
+    void twoSumEmptyInput() {
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        int[] sum = Solution.twoSum(new int[]{}, 9);
+        assertEquals(0, sum.length);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"2,7,11,15;9;0,1",
+                        "1,100,2,300,4,500;504;4,5",
+                        "10,17,9,18,7,16,11;21;0,6"}, delimiterString = ";")
+    @DisplayName("Should return the solution for a given array")
+    void twoSum(@ConvertWith(IntArrayConverter.class) int[] input, int target, @ConvertWith(IntArrayConverter.class) int[] expected) {
+
+        int[] sum = Solution.twoSum(input, target);
+        assertEquals(2, sum.length);
+        assertArrayEquals(expected, sum);
+    }
 }
